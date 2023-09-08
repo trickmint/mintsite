8b54e80ca9eaf290760cfbe56bd951abafd41c92
Luke <luke@nightcat.gg>
2023-09-08 00:04:29 -0400

    Hide volume number if left alone
    This clears the volume number text after 3 seconds of not fiddling with the volume.

--- customplayer.js
+++ customplayer.js
@@ -8,8 +8,19 @@ function skipSong() {
 	myAudio.currentTime=9999999;
 }
 
+// Hide volume number if left alone
+let timer; // Declare a variable to store the timer
+function setTimer() {
+    if (timer) {clearTimeout(timer)}  // Clear the previous timer
+    // Set a new timer
+    timer = setTimeout(function () {
+        document.getElementById("vol").innerHTML=""; // Clear volume text
+    }, 3000); // after 3 seconds
+}
+
 function thisVolume(volume_value) {
     var myvideo = document.getElementById("song");
     document.getElementById("vol").innerHTML=volume_value;
-    myvideo.volume = volume_value / 100;   
+    myvideo.volume = volume_value / 100;
+    setTimer();
 }
